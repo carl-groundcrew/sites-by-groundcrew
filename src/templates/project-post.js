19f4e60cca7080c36dab305bef955a3849914786
Carl Beaverson <carlbeaverson@gmail.com>
2020-05-26 10:28:28 +1000

    Change to CMS

--- src/templates/project-post.js
+++ src/templates/project-post.js
@@ -68,10 +68,14 @@ class ProjectPostTemplate extends React.Component {
             {post.frontmatter.galleryImages.map((node, i) => { 
               return (
                 <div className='project-asset' key={i}>
-                  <Image className='mobile-hide' fluid={node.image.childImageSharp.fluid}
+                  {node.image && (
+                    <Image className='mobile-hide' fluid={node.image.childImageSharp.fluid}
                     alt={'Project Image '+(i)} style={{ width: '100%'}} />
-                  <Image className='mobile-show' fluid={node.mobileImage.childImageSharp.fluid}
+                  )}
+                  {node.mobileImage && (
+                    <Image className='mobile-show' fluid={node.mobileImage.childImageSharp.fluid}
                     alt={'Mobile Project Image '+(i)} style={{ width: '100%'}} />
+                  )}
                   {node.video && (
                     <video className='mobile-hide' style={{ width: '100%'}} autoPlay muted loop>
                       <source src={node.video} type="video/mp4" />
