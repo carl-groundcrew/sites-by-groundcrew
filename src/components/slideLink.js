e33b64fe476457c8215d880970b9fd9a98571cce
Carl Beaverson <carlbeaverson@gmail.com>
2020-06-15 15:25:25 +1000

    Change to slide

--- src/components/slideLink.js
+++ src/components/slideLink.js
@@ -5,6 +5,7 @@ import {TimelineLite} from 'gsap'
 class pageLink extends React.Component {
   
   exit(exit, node) {
+    document.querySelector('.tl-edges').style.overflowX = 'hidden'
     document.documentElement.style.pointerEvents = 'none'
     document.documentElement.style.overflow = 'hidden'
     const el = new TimelineLite({paused: true});
@@ -15,7 +16,7 @@ class pageLink extends React.Component {
     const tl = new TimelineLite({paused: true});
     tl.fromTo( node.querySelector('.app'), { y: '95vh', height:'100vh', overflow:'hidden'}, { y: 0, ease:'power2.inOut', duration:entry.length/2})
     tl.from( node.querySelectorAll('.header-component'), { x: -50, ease: 'power1.out', duration:entry.length/3, onComplete:this.afterAnimation})
-    tl.fromTo( node.querySelector('.project-byline'), {y:0, opacity:0 }, { opacity:1, y: 0, ease: 'power1.out', duration:entry.length/3}, '-='+entry.length/3)
+    tl.fromTo( node.querySelector('.project-byline'), {x:15, opacity:0 }, { opacity:1, x: 0, ease: 'power1.out', duration:entry.length/3}, '-='+entry.length/3)
     return tl.play();
   }
   
