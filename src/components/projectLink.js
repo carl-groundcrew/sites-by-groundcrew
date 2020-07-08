553353bf91808fcfd7d83aa97588e5590b1e2a22
Carl Beaverson <carlbeaverson@gmail.com>
2020-07-08 16:46:43 +1000

    Change to project links

--- src/components/projectLink.js
+++ src/components/projectLink.js
@@ -1,51 +1,53 @@
 import React from "react"
 import TransitionLink from "gatsby-plugin-transition-link"
-import gsap from 'gsap'
+import {TimelineLite} from 'gsap'
 
 class projectLink extends React.Component {
   
   exit(exit, node) {
-
+    document.querySelector('.tl-edges').style.overflowX = 'hidden'
+    document.documentElement.style.pointerEvents = 'none'
+    document.documentElement.style.overflow = 'hidden'
+    node.querySelector('.app').style.overflowY = 'hidden'
+    node.querySelector('.app').style.fixed = 'fixed'
+    node.querySelector('.app').style.top = window.scrollY*-1
+    
     var offsetY = node.querySelector('.single-project[data-title="'+this.props.project+'"]').offsetTop;
-     
+
     window.scroll({
       top: offsetY,
       behavior: 'smooth'
     });
 
-    const tl = gsap.timeline({paused: true});
-    tl.to( node.querySelectorAll('.header-component'), { x: -50, ease: 'power1.out', duration:exit.length/3, delay:exit.length/3})
-    tl.to( node.querySelector('.fade-out-right'), { x:10, opacity:0, ease: 'power1.out', duration:exit.length/3}, '-='+exit.length/3)
-    tl.to( node.querySelector('.fade-out-left'), { x:-10, opacity:0, ease: 'power1.out', duration:exit.length/3}, '-='+exit.length/3)
-    tl.to( node.querySelectorAll('.app'), { opacity: 0, ease: 'power1.out', duration:exit.length/3})
-    return tl.play();
+    //const el = new TimelineLite({paused: true});
+    //el.fromTo( node.querySelector('.app'), { overflowY:'hidden',position:'fixed',  left:'0px', width:'100vw', top:window.scrollY*-1}, { ease:'power2.inOut', duration:exit.length/2});
+    //return el.play();
   }
-
   enter(entry, node) {
-    document.body.classList.remove('no-scroll');
-    const tl = gsap.timeline({paused: true});
-    tl.from( node.querySelector('.app'), { opacity: 0, ease: 'power1.in',duration:entry.length/2})
-    tl.from( node.querySelectorAll('.header-component'), { x: -50, ease: 'power1.out', duration:entry.length/3})
-    tl.fromTo( node.querySelector('.fade-in-left'), {x:10, opacity:0 }, { opacity:1, x: 0, ease: 'power1.out', duration:entry.length/3}, '-='+entry.length/3)
+    const tl = new TimelineLite({paused: true});
+    tl.fromTo( node.querySelector('.app'), { y: '0', height:'100vh', overflow:'hidden'}, { y: 0, ease:'power2.inOut', duration:entry.length/2})
+    tl.from( node.querySelectorAll('.header-component'), { x: -50, ease: 'power1.out', duration:entry.length/3, onComplete:this.afterAnimation})
     tl.fromTo( node.querySelector('.project-byline'), {y:15, opacity:0 }, { opacity:1, y: 0, ease: 'power1.out', duration:entry.length/3}, '-='+entry.length/3)
     return tl.play();
   }
+  
+  afterAnimation() {
+    var app = document.querySelector('.app');
+    app.classList.add('released');
+    app.style.overflow = 'unset';
+    document.documentElement.style.pointerEvents = 'auto'
+    document.documentElement.style.overflow = 'auto'
+    document.querySelector('.tl-edges').style.overflowX = 'unset'
+  }
 
   render() {
-
-    const timing = 2;
-
-    const arrow = ()=>{
-      if(this.props.arrow){
-        return <svg width="15" height="13" viewBox="0 0 15 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M11.068 9.35366H0V0H0.436893V8.87805H11.068V5.23171L15 9.19512L11.068 13V9.35366Z"/>
-                </svg>
-      }  
-    }
+    const timing = 2.5
     return (
-      <TransitionLink className='page-link' exit={{ length:timing, trigger: ({ exit, node }) => this.exit(exit, node)}} entry={{ delay:timing, length:timing, trigger: ({ entry, node }) => this.enter(entry, node)}} to={this.props.to}>
+      <TransitionLink className="link"
+      exit={{ length:timing, direction: this.props.direction, trigger: ({ exit, node }) => this.exit(exit, node)}}
+      entry={{ length:timing, delay:0.4, direction: this.props.direction, trigger: ({ entry, node }) => this.enter(entry, node)}}
+      to={this.props.to}>
         {this.props.children}
-        {arrow()}
       </TransitionLink>
     );
   }
